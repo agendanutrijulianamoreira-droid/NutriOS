98b3cfd0468f49dbae95a744a50a44e8af857f79
agendanutrijulianamoreira-droid <agendanutrijulianamoreira@gmail.com>
2026-08-29 10:58:42 -0300

    feat: expose nutrition library workspace

---
+++ src/app/nutrition-library/page.tsx
@@ -0,0 +1,5 @@
+import { NutritionLibraryScreen } from "@/modules/nutrition-engine/ui/nutrition-library-screen";
+
+export default function NutritionLibraryPage() {
+  return <NutritionLibraryScreen />;
+}
